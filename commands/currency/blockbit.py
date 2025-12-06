e3238bcd0e4646a74ccd5ab93f41be28270327b5
6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-06 21:25:05 +0000

    Make a basic response fetcher

--- commands/currency/blockbit.py
+++ commands/currency/blockbit.py
@@ -7,3 +7,10 @@ cloud = session.connect_cloud(669020072)
 def request_search(username):
   """ Send a request to get user's balance. """
   cloud.set_var("TO_HOST", Encoding.encode(f"search&{username}"))
+
+def get_response():
+  """ Get response for the last request. """
+  i = 1
+  vars = []
+  while i not == 10:
+    vars.append(cloud.get_var(f"FROM_HOST{i}")
