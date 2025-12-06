62dfcb1b527538a6cae32eef440b904e17bf828c
Fluffyscratch <fluffygamesproductions@gmail.com>
2025-12-06 17:47:51 +0100

    Refactor /randomprojects to stop relying on ESDB (they shut down) and update /christmas

--- commands/search_commands.py
+++ commands/search_commands.py
@@ -2,6 +2,8 @@
 Search and discovery slash commands (ESDB-powered and others).
 """
 
+import random
+
 import discord
 from discord import app_commands
 import requests
@@ -13,79 +15,52 @@ from config import bot, scratch_orange
 
 @bot.tree.command(
     name="randomprojects",
-    description="Shows a number of random scratch projects, powered by ESDB.",
+    description="Shows a number of random scratch projects.",
 )
 async def randomprojects(interact: discord.Interaction, number: int):
-    response = requests.get(
-        f"https://explore.eletrix.fr/api/projects/random?limit={number}"
-    )
+    await interact.response.defer()
 
-    if response.status_code == 200:
-        message = ""
-        for item in response.json():
-            message = f"{message} [**{item['title']}**](https://scratch.mit.edu/projects/{item['id']}) :\n\n"
+    message = ""
+    max_project_id = scratch.total_site_stats().get('PROJECT_COUNT')
+    for i in range(number):
+        while True:
+            try:
+                project = scratch.get_project(
+                    random.randint(1, max_project_id)
+                )
+            except scratch.utils.exceptions.ProjectNotFound:
+                continue
+            break
+        message = f"{message} [**{project.title}**](https://scratch.mit.edu/projects/{project.id})\n\n"
 
-        msg = discord.Embed()
-        if number == 1:
-            msg.title = "Here is 1 random project !"
-        else:
-            msg.title = f"Here are {number} random projects !"
-        msg.description = message
-        msg.color = scratch_orange
-        await interact.response.send_message(embed=msg)
+    msg = discord.Embed()
+    if number == 1:
+        msg.title = "Here is 1 random project !"
     else:
-        await interact.response.send_message(
-            embed=discord.Embed(
-                title=f"Whoops, looks like we've got an error {response.status_code} !<:giga404:1330551323610976339>",
-                color=discord.Color.red(),
-            )
-        )
-
-
-@bot.tree.command(
-    name="bettersearch",
-    description="Allows you to search projects without bugs such as <900 million ids. Powered by ESDB.",
-)
-async def bettersearch(interact: discord.Interaction, query: str):
-    response = requests.get(
-        f"https://explore.eletrix.fr/api/projects/search?search={query}"
-    )
-
-    if response.status_code == 200:
-        message = ""
-        for item in response.json():
-            message = f"{message} [**{item['title']}**](https://scratch.mit.edu/projects/{item['id']}) :\n\n"
-
-        msg = discord.Embed()
-        msg.title = f"<:search:1333037655902130247>Results for '{query}' :"
-        msg.description = message
-        msg.color = scratch_orange
-        await interact.response.send_message(embed=msg)
-    else:
-        await interact.response.send_message(
-            embed=discord.Embed(
-                title=f"Whoops, looks like we've got an error {response.status_code} !<:giga404:1330551323610976339>",
-                color=discord.Color.red(),
-            )
-        )
+        msg.title = f"Here are {number} random projects !"
+    msg.description = message
+    msg.color = scratch_orange
+    await interact.followup.send(embed=msg)
 
 
 @bot.tree.command(
     name="christmas", description="Take a look at the best christmas projects easily !"
 )
 async def christmas(interact: discord.Interaction):
+    await interact.response.defer()
+
     message = ""
     projects = scratch.search_projects(
-        query="christmas", mode="trending", language="en", limit=10, offset=0
+        query="christmas", mode="popular", language="en", limit=10, offset=0
     )
     for item in projects:
         message = (
-            f"{message}\n\n **[{item.title}](<https://scratch.mit.edu/projects/{item.id}>)** "
-            f"by [{item.author().username}](scratch.mit.edu/users/{item.author().username})"
+            f"{message}\n\n **[{item.title}](<https://scratch.mit.edu/projects/{item.id}>)**\n"
+            f"-# by [{item.author().username}](https://scratch.mit.edu/users/{item.author().username})"
         )
-    await interact.response.send_message(
+    await interact.followup.send(
         embed=discord.Embed(
-            title="<:SantaCat:1444277069826494557>Top 10 trending christmas projects<:SantaCat:1444277069826494557> :",
+            title="<:SantaCat:1444277069826494557>Top 10 popular christmas projects<:SantaCat:1444277069826494557> :",
             description=message,
             color=scratch_orange,
         )
