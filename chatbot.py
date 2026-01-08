73c066f163eab1431caa8ce0bd4da92893011c7f
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2026-01-08 19:46:46 +0000

    production ready ai

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,21 @@
 import scratchattach as sa
 import requests
 import time
+from urllib.parse import quote
+
 
 index = 0
 
+# function to encode into url format
 def encode(text: str):
     """
-    TODO: FIX: Function to encode special character into wen url format. 
+    Function to encode special character into wen url format. 
 
     @parameter:
 
         - text: the text to be encoded. 
     """
-    encoded = encoded.replace("?", "%3D") # more here...
+    encoded = quote(text, safe="?-_.!~*'()")
     
     return encoded
     
@@ -30,9 +33,9 @@ def answer(query: str, username: str):
     print("Answering...")
     prompt = { "content": "You are a helpful scratch.mit.edu user and bot called ScratchOn. You cannot swear or do anything inappropriate. Never say anything bad about someone or something. Your language must be appropriate for 7-12 year olds. Always refer to yourself as ScratchOn, or _Scratch-On_ as your scratch username. Either refer to the user by their username, or don't refer to them at all. Keep your answers short and concise, up to 500 characters. Do not use regular emojis, only use Scratch-style emojis like :), _:D_, or XD. Speak like a scratch.mit.edu user would, using simple words and phrases a 12 years old would use in a chat, for example 'Hey!', 'That's cool!', 'Thanks!', 'No problem!', 'I guess you're right lol'. Answer the following user with their question/message." }
 
-    res = requests.get(f"https://text.pollinations.ai/{encode(prompt + " : " + username + " : " + query)}")
+    res = requests.get(f"https://text.pollinations.ai/{encode(prompt.content + " : " + username + " : " + query)}")
     result = res.json()
-    print("Answered !")
+    print("Answered!")
     return result
 
 
