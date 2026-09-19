cfbb24bbe40fb4d54bf749898e785591ab3255a2
Fluffyscratch <fluffygamesproductions@gmail.com>
2026-09-19 18:27:53 +0200

    Make activity stats shared between bot events and utility commands

--- events/bot_events.py
+++ events/bot_events.py
@@ -7,7 +7,8 @@ import interactions
 import logging
 from interactions.api.events import CommandError
 
-from config import bot, bot_statuses, button_states, bot_ready, activity_stats
+import config
+from config import bot, bot_statuses, button_states, bot_ready
 from database import add_server
 
 
@@ -51,50 +52,51 @@ class BotEvents(interactions.Extension):
         """Handle member rich presence update events."""
         if before.activities != after.activities:
             # Handle activity changes to update stats
+            stats = config.activity_stats
             for newactivity, oldactivity in zip(after.activities, before.activities):
                 # Handle new activities
                 if newactivity.name == "TurboWarp Desktop":
                     if newactivity.type == interactions.ActivityType.PLAYING:
-                        activity_stats["turbowarp"] += 1
+                        stats["turbowarp"] += 1
                     elif newactivity.type == interactions.ActivityType.STREAMING:
-                        activity_stats["streaming_turbowarp"] += 1
+                        stats["streaming_turbowarp"] += 1
                 elif match(r"Scratch 3\.\d+\.\d+", newactivity.name):
                     if newactivity.type == interactions.ActivityType.PLAYING:
-                        activity_stats["scratch3"] += 1
+                        stats["scratch3"] += 1
                     elif newactivity.type == interactions.ActivityType.STREAMING:
-                        activity_stats["streaming_scratch3"] += 1
+                        stats["streaming_scratch3"] += 1
                 elif (
                     newactivity.name == "Scratch 2 Offline Editor"
                     and newactivity.type == interactions.ActivityType.PLAYING
                 ):
-                    activity_stats["scratch2"] += 1
+                    stats["scratch2"] += 1
                 elif (
                     match(r"Scratch 1\.4 of \d{2}-[A-Za-z]{3}-\d{2}", newactivity.name)
                     and newactivity.type == interactions.ActivityType.PLAYING
                 ):
-                    activity_stats["scratch1"] += 1
+                    stats["scratch1"] += 1
 
                 # Handle removed activities
                 if oldactivity.name == "TurboWarp Desktop":
                     if oldactivity.type == interactions.ActivityType.PLAYING:
-                        activity_stats["turbowarp"] -= 1
+                        stats["turbowarp"] -= 1
                     elif oldactivity.type == interactions.ActivityType.STREAMING:
-                        activity_stats["streaming_turbowarp"] -= 1
+                        stats["streaming_turbowarp"] -= 1
                 elif match(r"Scratch 3\.\d+\.\d+", oldactivity.name):
                     if oldactivity.type == interactions.ActivityType.PLAYING:
-                        activity_stats["scratch3"] -= 1
+                        stats["scratch3"] -= 1
                     elif oldactivity.type == interactions.ActivityType.STREAMING:
-                        activity_stats["streaming_scratch3"] -= 1
+                        stats["streaming_scratch3"] -= 1
                 elif (
                     oldactivity.name == "Scratch 2 Offline Editor"
                     and oldactivity.type == interactions.ActivityType.PLAYING
                 ):
-                    activity_stats["scratch2"] -= 1
+                    stats["scratch2"] -= 1
                 elif (
                     match(r"Scratch 1\.4 of \d{2}-[A-Za-z]{3}-\d{2}", oldactivity.name)
                     and oldactivity.type == interactions.ActivityType.PLAYING
                 ):
-                    activity_stats["scratch1"] -= 1
+                    stats["scratch1"] -= 1
 
     # ------------------------------------------------------------------ #
     # Component interaction handler (settings buttons)                    #
